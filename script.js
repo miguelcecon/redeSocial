b5d6564b7508052897fa7c32692942a9f8d9610b
miguelcecon <miguelceconsilva@gmail.com>
2026-09-01 09:02:39 -0300

    abvfjhadvbf

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".like-Btn");
+    const postMedia = document.querySelector (".post-media");
+if (!likeBtn) return; 
+
+const likesCountSpan = liekBtn.querySelector (".likes-count");
+const bookmarkBtn = document.querySelector (".bookmark-btn")
+
+
+
+
+
+
+})
