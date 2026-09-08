0221a663c04753c4d6a36fdde7df1216f896cf6d
miguelcecon <miguelceconsilva@gmail.com>
2026-09-08 09:00:21 -0300

    sdfgsdfg

--- script.js
+++ script.js
@@ -6,9 +6,32 @@ if (!likeBtn) return;
 const likesCountSpan = liekBtn.querySelector (".likes-count");
 const bookmarkBtn = document.querySelector (".bookmark-btn")
 
+let IsLiked = false;
+let baseLikes = 0;
 
+if (likesCountSpan){
+    likesCountSpan.textContent = "0";
+}
 
+//números grandes
 
+function formatLikes(num) {
+    if(num >=1000){
+        return (num/1000).toFixed(1)+"K";
+    }
+    return num.toString();
 
+}
+
+function addLike(){
+    basedLikes++; 
+    isLiked = true;
+    likeBtn.classList.add("liked");
+
+    if(likesCountSpan){
+        likesCountSpan.textContent = formatLikes(baseLikes);
+    }
+    }
+}
 
 })
